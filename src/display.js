0cc9e34dab0e284c12e73d9d7e3ef3d93c0edf9e
Krzysztof Hain <krzysztof.hain@thingtrunk.com>
2025-04-17 13:05:05 +0200

    Add test grid display to DOM

--- src/display.js
+++ src/display.js
@@ -1,3 +1,7 @@
+/* global document */
+
+import { BOARD_WIDTH } from './gameboard.js';
+
 export class displayController {
     constructor (board1El, board2El) {
         this.board1 = new boardDisplay(board1El);
@@ -8,10 +12,29 @@ export class displayController {
 class boardDisplay {
     constructor (boardEl) {
         this.boardEl = boardEl;
+        this.spaces = [];
         this.initDisplay();
     }
 
     initDisplay () {
-        this.boardEl.textContent = 'hello world';
+        this.boardEl.style.gridTemplateRows = `repeat(${BOARD_WIDTH}, 1fr)`;
+        this.boardEl.style.gridTemplateColmuns = `repeat(${BOARD_WIDTH}, 1fr)`;
+        this.createSpaces();
     }
+
+    createSpaces () {
+       for (let x = 0; x < BOARD_WIDTH; x++) {
+            this.spaces.push(new Array());
+        
+            for (let y = 0; y < BOARD_WIDTH; y++) {
+                const spaceEl = document.createElement('div');
+                this.spaces[x].push(spaceEl);
+                spaceEl.style.gridRow = `${y+1} / span 1`;
+                spaceEl.style.gridColumn = `${x+1} / span 1`;
+                spaceEl.textContent = `${x}, ${y}`;
+                this.boardEl.append(spaceEl);
+            }
+        } 
+    }
+    
 }
--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,6 @@
 import { Ship } from "./ship.js";
 
-const BOARD_WIDTH = 10;
+export const BOARD_WIDTH = 10;
 const SHIP_LENGTHS = [5, 4, 3, 3, 2];
 
 class Space {
