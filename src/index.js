ce085d6e14836b4c4295b366182504fb2ddf56f2
Krzysztof Hain <krzysztof.hain@thingtrunk.com>
2025-04-17 23:32:42 +0200

    Refactor printBoards call

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const board1El = document.getElementById('board1');
 const board2El = document.getElementById('board2');
 const player1NameEl = document.getElementById('player1-name');
 const player2NameEl = document.getElementById('player2-name');
+const playerBoards = [game.player1.board.spaces, game.player2.board.spaces];
 
 const display = new displayController(board1El, player1NameEl, board2El, player2NameEl);
 
 display.boardDisplay1.setPlayerName(game.player1.name);
 display.boardDisplay2.setPlayerName(game.player2.name);
-//display.refreshBoards();
 
-display.printBoards([game.player1.board.spaces, game.player2.board.spaces]);
+display.printBoards(playerBoards);
