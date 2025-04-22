7c954787a6a1a8e28bddcd7cf5b5e5a19c469eb4
Krzysztof Hain <krzysztof.hain@thingtrunk.com>
2025-04-23 00:36:31 +0200

    Improve drawDomElement function
    * Fixed input with no container
    * Added option to create img element

--- src/dom-fns.js
+++ src/dom-fns.js
@@ -5,12 +5,15 @@ data.type type of new DOM element
 data.container Container to append new element to
 data.classes Array of CSS classes to add to the element
 data.text TextContent of the element
+data.src src attribute of an image
 */
 export function drawDomElement(data) {
     const el = document.createElement(data.type);
 
-    data.container.appendChild(el);
-
+    if (data.container) {
+        data.container.appendChild(el); 
+    }
+    
     if (data.classes.length) {
         addCssClasses(el, data.classes);
     }
@@ -19,20 +22,13 @@ export function drawDomElement(data) {
         el.textContent = data.text;
     }
 
+    if (data.type === 'img' && data.src) {
+        el.src = data.src;
+    }
+
     return el;
 }
 
-/*
-export const drawImgElement = (imgSrc, container, cssClasses) => {
-  const domEl = document.createElement('img');
-  container.appendChild(domEl);
-  domEl.src = imgSrc;
-  if (cssClasses !== undefined && cssClasses.length) {
-    addCssClasses(domEl, cssClasses);
-  }
-  return domEl;
-};
-*/
 
 function addCssClasses(el, classes) {
     for (let css of classes) {
