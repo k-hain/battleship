4098d1bf137663879d1a406fb043de6ea07efd67
Krzysztof Hain <krzysztof.hain@thingtrunk.com>
2025-04-26 13:32:03 +0200

    Improve script formatting

--- src/display-controller.js
+++ src/display-controller.js
@@ -8,7 +8,7 @@ import {
     REFRESH_DISPLAY_AND_WIDGETS,
     START_SHIP_MOVEMENT,
     PLACE_SHIP,
-    ROTATE_SHIP
+    ROTATE_SHIP,
 } from './event-types';
 
 export class DisplayController {
@@ -112,7 +112,7 @@ export class DisplayController {
     }.bind(this);
     placeShipToken = PubSub.subscribe(PLACE_SHIP, this.placeShipAndRefresh);
 
-    roateShip = function(msg, data) {
+    roateShip = function (msg, data) {
         const board = this.boards[data.id];
         const rotated = board.data.rotateShip(data.ship);
 
